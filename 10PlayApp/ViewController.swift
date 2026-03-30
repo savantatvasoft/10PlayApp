dee7e61f2d24cf486439dab4ff139980463dbb89
savantatvasoft <savan.soni@tatvasoft.com>
2026-03-30 13:15:21 +0530

    Initial Commit

---
+++ 10PlayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10PlayApp
+//
+//  Created by MACM72 on 30/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
