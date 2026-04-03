a19b67f82e630de6a36852e6f21983b6a933c115
savansamar <savansoni125@gmail.com>
2026-04-03 13:20:59 +0530

    Add edit UI fields in Profile

--- 10PlayApp/Feature/Account/View/AccountVC.swift
+++ 10PlayApp/Feature/Account/View/AccountVC.swift
@@ -8,19 +8,66 @@
 import UIKit
 
 class AccountVC: BaseViewController {
-
+    
     @IBOutlet weak var header: Header!
+    @IBOutlet weak var firstName: InputField!
+    @IBOutlet weak var lastName: InputField!
+    @IBOutlet weak var phoneNumber: InputField!
+    @IBOutlet weak var email: InputField!
+    @IBOutlet weak var password: InputField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        header.centerIcon.isHidden = true
-        header.centerTitle.isHidden = false
-        header.configure(headerTitle: "account_title".localized,rightIcon: UIImage(named: "ic_10_play_white"))
+        setupFields()
         attachHeader(header)
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
+}
 
+extension AccountVC {
+    private func setupFields() {
+        
+        header.centerIcon.isHidden = true
+        header.centerTitle.isHidden = false
+        header.configure(
+            headerTitle: "account_title".localized,
+            rightIcon: UIImage(named: "ic_10_play_white")
+        )
+        
+        firstName.configure(
+            title: "first_name_title".localized,
+            value: "shahh"
+        )
+        
+        // Last Name
+        lastName.configure(
+            title: "last_name_title".localized,
+            value: "Kruti"
+        )
+        
+        // Phone Number
+        phoneNumber.configure(
+            title: "phone_number_title".localized,
+            value: "9428151966"
+        )
+        
+        email.rightButton.isHidden = true
+        email.configure(
+            title: "email_title".localized,
+            value: "kruti.shah@yopmail.com",
+            isEditable: false
+        )
+        
+        password.configure(
+            title: "password_title".localized,
+            value: "12345678",
+            isPassword: true
+        )
+        
+    }
+    
+    
 }
--- 10PlayApp/Feature/Account/View/InputField.swift
+++ 10PlayApp/Feature/Account/View/InputField.swift
@@ -7,19 +7,17 @@
 
 import UIKit
 
-class InputField: UIView {
+class InputField: UIView, UITextFieldDelegate {
 
+    @IBOutlet weak var mainView: UIView!
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var topLabel: UILabel!
- 
     @IBOutlet weak var titleLabel: UITextField!
-    
     @IBOutlet weak var rightButton: UIButton!
     
+    private let bottomBorder = CALayer()
     
-    @IBAction func onPressRigthIcon(_ sender: Any) {
-    }
-    
+    // MARK: - Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
@@ -30,12 +28,75 @@ class InputField: UIView {
         commonInit()
     }
 
-    // MARK: - Load XIB
+    // MARK: - Lifecycle
     private func commonInit() {
         Bundle.main.loadNibNamed("InputField", owner: self, options: nil)
         addSubview(contentView)
-        contentView.frame = bounds
-        contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: self.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+        ])
+
+        titleLabel.delegate = self
+        setupStyle()
+    }
+    
+    private func setupStyle() {
+        bottomBorder.backgroundColor = UIColor.systemGray4.cgColor
+        mainView.layer.addSublayer(bottomBorder)
+        
+        topLabel.font = AppFont.get(.extraBold, size: 13)
+        titleLabel.font = AppFont.get(.regular, size: 13)
+        mainView.clipsToBounds = false
+        
+        // Ensure the field starts as non-editable
+        titleLabel.isUserInteractionEnabled = false
     }
 
+    func configure(title: String,
+                   value: String?,
+                   placeholder: String = "",
+                   isEditable: Bool = true,
+                   isPassword: Bool = false) {
+        
+        topLabel.text = title.uppercased()
+        titleLabel.text = value
+        titleLabel.placeholder = placeholder
+        titleLabel.isSecureTextEntry = isPassword
+       
+    }
+
+    // MARK: - Actions
+    @IBAction func onPressRigthIcon(_ sender: UIButton) {
+        titleLabel.isUserInteractionEnabled = true
+        titleLabel.becomeFirstResponder()
+    }
+    
+    // MARK: - UITextFieldDelegate
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.isUserInteractionEnabled = false
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    // MARK: - Layout
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        mainView.layoutIfNeeded()
+        
+        let borderHeight: CGFloat = 1.0
+        bottomBorder.frame = CGRect(
+            x: 0,
+            y: mainView.frame.height - borderHeight,
+            width: mainView.frame.width,
+            height: borderHeight
+        )
+    }
 }
